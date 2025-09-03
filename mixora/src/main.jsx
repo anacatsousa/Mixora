73c16e6517427fb5a585966634f58bcd0a586543
Catarina Sousa <catarinaasou@gmail.com>
2025-09-03 01:35:05 +0100

    first commit

---
+++ mixora/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { Routes } from 'react-router';
+import { Route } from 'react-router';
+import './scss/style.scss';
+import HomePage from './pages/HomePage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
+);
