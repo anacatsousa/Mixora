8c0c9972acbe703d0be223b4f35b40545a92ec6c
Catarina Sousa <catarinaasou@gmail.com>
2025-09-10 22:33:19 +0100

    add header & back button changes

--- mixora/src/components/BackButton/BackButton.jsx
+++ mixora/src/components/BackButton/BackButton.jsx
@@ -2,13 +2,13 @@ import { useNavigate } from 'react-router';
 import { ChevronLeft } from 'lucide-react';
 import './_backButton.scss';
 
-function BackButton() {
+function BackButton({ absolute = false }) {
 	const navegate = useNavigate();
 
 	const handleBack = () => navegate(-1);
 
 	return (
-		<button onClick={handleBack} className="back-button">
+		<button onClick={handleBack} className={`back-button ${absolute ? 'back-button--absolute' : ''}`}>
 			<ChevronLeft className="back-button__icon" />
 			Back
 		</button>
