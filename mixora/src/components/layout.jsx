c9e5dc8eb200cc394eb09c4d270a90384d6c3211
Catarina Sousa <catarinaasou@gmail.com>
2025-09-13 01:15:42 +0100

    change cart page to a modal

--- mixora/src/components/layout.jsx
+++ mixora/src/components/layout.jsx
@@ -2,15 +2,20 @@ import { Outlet } from 'react-router';
 import Newsletter from './Newsletter/Newsletter';
 import Footer from './Footer/Footer';
 import NavBar from './NavBar/NavBar';
+import { useState } from 'react';
+import CartModal from './CartModal/CartModal';
 
 function Layout() {
+	const [isCartOpen, setIsCartOpen] = useState(false);
+
 	return (
 		<>
 			<header>
-				<NavBar />
+				<NavBar onCartClick={() => setIsCartOpen(true)} />
 			</header>
 			<main>
 				<Outlet />
+				<CartModal isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
 			</main>
 			<footer>
 				<Newsletter />
