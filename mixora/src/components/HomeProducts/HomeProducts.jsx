a75ca0bc80dc1ee0194757502246ddbac390e18c
Catarina Sousa <catarinaasou@gmail.com>
2025-09-15 23:32:18 +0100

    add slide on cards

--- mixora/src/components/HomeProducts/HomeProducts.jsx
+++ mixora/src/components/HomeProducts/HomeProducts.jsx
@@ -3,9 +3,32 @@ import useProducts from '../../hooks/useProducts';
 import './_homeproducts.scss';
 import Card from '../Card/Card';
 import Container from '../Container/Container';
+import { useRef } from 'react';
+import Button from '../Button/Button';
 
 function HomeProducts() {
 	const { products, isLoading } = useProducts();
+	const scrollRef = useRef(null);
+
+	const oneProductPerCategory = Object.values(
+		products.reduce((acc, product) => {
+			if (!acc[product.category.name]) {
+				acc[product.category.name] = product;
+			}
+			return acc;
+		}, {})
+	);
+
+	const scroll = (direction) => {
+		const container = scrollRef.current;
+		if (container) {
+			const scrollAmount = 370;
+			container.scrollBy({
+				left: direction === 'left' ? -scrollAmount : scrollAmount,
+				behavior: 'smooth',
+			});
+		}
+	};
 
 	if (isLoading === true) return <span>LOADING...</span>;
 	return (
@@ -13,14 +36,11 @@ function HomeProducts() {
 			<Container>
 				<span className="wrapper__title">[ NEW IN ]</span>
 				<div className="wrapper__products">
-					{Object.values(
-						products.reduce((acc, product) => {
-							if (!acc[product.category.name]) {
-								acc[product.category.name] = product;
-							}
-							return acc;
-						}, {})
-					).map((product) => (
+					<div className="wrapper-controls">
+						<Button>&lsaquo;</Button>
+						<Button>&rsaquo;</Button>
+					</div>
+					{oneProductPerCategory.map((product) => (
 						<Link to={`category/${product.category.slug}/${product.slug}`} key={product.id}>
 							<Card img={product.images[0]} title={product.title} price={product.price} category={product.category.name} />
 						</Link>
