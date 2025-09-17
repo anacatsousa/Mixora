ee23efa98dff37711ee137400a0601ac6ecf685b
Catarina Sousa <catarinaasou@gmail.com>
2025-09-17 15:47:38 +0100

    page all products done

--- mixora/src/pages/AllProductsPage.jsx
+++ mixora/src/pages/AllProductsPage.jsx
@@ -1,7 +1,13 @@
+import AllProducts from '../components/AllProducts/AllProducts';
 import HeaderCategoryProducts from '../components/HeaderCategoryproducts/HeaderCategoryproducts';
 
 function AllProductsPage() {
-	return <HeaderCategoryProducts title="All" />;
+	return (
+		<>
+			<HeaderCategoryProducts title="All Products" />
+			<AllProducts />
+		</>
+	);
 }
 
 export default AllProductsPage;
