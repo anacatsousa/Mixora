7e3bef79e70d560ca41efcf7669572a778f2ec77
Catarina Sousa <catarinaasou@gmail.com>
2025-09-18 14:17:15 +0100

    when api has no imgs

--- mixora/src/components/HeaderProductPage/HeaderProductPage.jsx
+++ mixora/src/components/HeaderProductPage/HeaderProductPage.jsx
@@ -2,11 +2,21 @@ import useProducts from '../../hooks/useProducts';
 import BackButton from '../BackButton/BackButton';
 import Loading from '../Loading/Loading';
 import './_headerProductPage.scss';
+import noPhoto from '@/assets/no_photo.png';
 
 function HeaderProductPage({ categorySlug, productSlug }) {
 	const { products, isLoading } = useProducts();
 
 	const product = products.find((product) => product.category.slug === categorySlug && product.slug === productSlug);
+
+	function isValidImg(src) {
+		if (typeof src !== 'string') return false;
+
+		const trimmed = src.trim().toLowerCase();
+		const validExtensions = ['.jpg', '.jpeg', '.png', '.webp'];
+
+		return trimmed !== '' && trimmed !== 'undefined' && validExtensions.some((ext) => trimmed.endsWith(ext));
+	}
 
 	if (isLoading === true) return <Loading />;
 	if (!products || products.length === 0) return <span> no product</span>;
@@ -15,7 +25,11 @@ function HeaderProductPage({ categorySlug, productSlug }) {
 		<section className="header-products">
 			<BackButton absolute={true} />
 			{product.images.map((img) => (
-				<img src={img} alt={product.slug} className="header-products__images" />
+				<img
+					src={isValidImg(img) ? img : noPhoto}
+					alt={product.slug}
+					className={`header-products__images ${img === 2 ? 'header-products__images--two' : img === 1 ? 'header-products__images--one' : ''}`}
+				/> //className="header-products__images"
 			))}
 		</section>
 	);
